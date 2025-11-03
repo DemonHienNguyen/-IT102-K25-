352b93e4a95a6b20c43e917ee1f3b6c2a2b03660
Demon :) <siuotp@gmail.com>
2025-11-03 20:45:23 +0700

    Update BTVN 7.c

--- [IT102-K25]_Session-09/BTVN 7.c
+++ [IT102-K25]_Session-09/BTVN 7.c
@@ -4,23 +4,22 @@
 
 int i, j;
 
-void inMaTranCheo(int N, int array[N][N]){
+void reversed(int N, int array[N]){
 
-int sum =0;
+int temp;
 
-	printf("\t----------VAY CAO SO CHEO LA----------\n");
-	for(i = 0; i < N;i++){
-		for(j =0; j < N; j++){
-			if(i==j){
-				printf("          %5d",array[i][j]);
-				sum += array[i][j];
-			}else{
-				printf("       ");
-			}
+	printf("\t----------SO DAO NGUOC----------\n");
+	for(i = 0; i < N /2;i++){
+		temp = array[i];
+		array[i] = array[N - i - 1];
+		array[N -i - 1] = temp;
 		}
 	printf("\n");
-	}
-printf("\t----------TONG CAC SO CHEO LA: %d", sum);
+printf("\t----------SO DAO NGUOC LA:\n");
+	for(i=0; i<N;i++){
+			printf("\t%5d", array[i]);
+		}
+	printf("\n");
 }
 
 
@@ -37,24 +36,22 @@ do{
 	}
 }while(n<=0);
 
-int maTran[n][n];
+int maTran[n];
 
 printf("\tnhap phan tu o vi tri: \n");
 	for(i=0; i<n;i++){
-		for(j=0;j<n;j++){
-			printf("array[%d][%d]: ", i, j);
-			scanf("%d", &maTran[i][j]);
-		}
+
+			printf("array[%d]: ", i);
+			scanf("%d", &maTran[i]);
 	}
 printf("\n");
 	
 printf("\t----------VAY MANG LA----------\n");
 	for(i=0; i<n;i++){
-		for(j=0;j<n;j++){
-			printf("\t%5d", maTran[i][j]);
+			printf("\t%5d", maTran[i]);
 		}
 	printf("\n");
-	}
-inMaTranCheo(n, maTran);
+
+reversed(n, maTran);
 return 0;
 }
